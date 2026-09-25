7baffdac4ad563d69d3a15ac77988557aadf892f
MiguelDuval <88213910+MiguelDuval@users.noreply.github.com>
2026-09-25 14:29:27 +0200

    feat: add src/core/TransportMath.h

---
+++ src/core/TransportMath.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace mozart::timing {
+
+[[nodiscard]] double beatAtTime(
+        double referenceBeat,
+        double referenceTimeSeconds,
+        double tempoBpm,
+        double timeSeconds) noexcept;
+
+[[nodiscard]] double quantizeBeat(
+        double beat,
+        double quantumBeats) noexcept;
+
+} // namespace mozart::timing
